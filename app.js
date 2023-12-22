3acda34ba1604591ed596a340e6f9919443ba528
Moonfraction <chandranshsingh@kgpian.iitkgp.ac.in>
2023-12-22 16:36:19 +0530

    website preview complete

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const songs = [
     {
         id: 1,
         name: `Perfect<br>
-        <div class="subtitle">Ed_Sheeran</div>` ,
+        <div class="subtitle">Ed Sheeran</div>` ,
         path: 'Assets/songs/1.mp3',
         coverPath: 'Assets/Cover/1.jpg',
         bgPath: 'Assets/Bg/1.jpg',
@@ -85,10 +85,88 @@ const songs = [
 ]
 
 
+//added song duration, name, coverImg to the song list
+Array.from(document.getElementsByClassName('songItem')).forEach((element, i) => {
+    //add time duration to each song
+    let tempaudio = new Audio(songs[i].path);
+    tempaudio.addEventListener('loadedmetadata', () => {
+    let min = Math.floor(tempaudio.duration / 60);
+    let sec = Math.floor(tempaudio.duration % 60);
+    if (sec < 10) {
+        sec = `0${sec}`;
+    }
+    if (min < 10) {
+        min = `0${min}`;
+    }
+    element.getElementsByClassName('timeStamp')[0].innerText = `${min}:${sec}`;
+    })
+    //add song name and cover image to each song
+    element.getElementsByTagName('h5')[0].innerHTML = songs[i].name;
+    element.getElementsByTagName('img')[0].src = songs[i].coverPath;
+})
+
+//add song name and cover image to each pop_item
 Array.from(document.getElementsByClassName('pop_item')).forEach((e, i) => {
     e.getElementsByTagName('img')[0].src = songs[i].coverPath;
     e.getElementsByTagName('h5')[0].innerHTML = songs[i].name;
 })
+
+
+//search data start
+let search_results = document.getElementsByClassName('search_results')[0];
+
+songs.forEach(element => {
+    const { id, name, coverPath } = element;
+    let card = document.createElement('a');
+    card.classList.add('card');
+    card.href = "#"+(id-1);
+    card.innerHTML = `<img src="${coverPath}" alt=""> <div class="content">${name}</div>`;
+
+    search_results.appendChild(card);
+
+    // card.onclick = () => {
+    //     music.src = songs[id - 1].path;
+    //     music.play();
+    //     masterPlay.classList.remove('fa-play-circle');
+    //     masterPlay.classList.add('fa-pause-circle');
+    //     poster_master_play.src = songs[id - 1].coverPath;
+    //     gif.style.opacity = 1;
+
+    //     let songTitles = songs.filter((els) => {
+    //         return els.id == id;
+    //     });
+    //     songTitles.forEach(elss => {
+    //         masterSongName.innerHTML = elss.name;
+    //     });
+    // }
+
+})
+
+
+let input = document.getElementsByTagName('input')[0];
+input.addEventListener('input', () => {
+    let results = document.getElementsByClassName('search_results')[0];
+    results.style.display = "flex";
+    results.style.flexDirection = "column";
+
+    let val = input.value;
+    Array.from(document.getElementsByClassName('card')).forEach((e) => {
+        let content = e.getElementsByClassName('content')[0].innerText;
+        if (content.toLowerCase().includes(val.toLowerCase())) {
+            e.style.display = "flex";
+        }
+        else {
+            e.style.display = "none";
+        }
+    })
+    if(val == ""){
+        results.style.display = "none";
+        Array.from(document.getElementsByClassName('card')).forEach((e) => {
+            e.style.display = "none";
+        })
+    }
+});
+//search data end
 
 
 let masterPlay = document.getElementById('masterPlay');
@@ -100,18 +178,21 @@ masterPlay.addEventListener('click', () => {
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
         gif.style.opacity = 1;
+        poster_master_play.style.opacity = 1;
     }
     else {
         music.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
         gif.style.opacity = 0;
+        // poster_master_play.style.opacity = 0;
     }
 })
 
 const makeAllBackgrounds = () => {
     Array.from(document.getElementsByClassName('pop_item')).forEach((element) => {
         element.style.background = "none";
+        element.getElementsByClassName('subtitle')[0].style.color = "gray";
     })
 }
 const makeAllPlays = () => {
@@ -126,6 +207,8 @@ const makeAllPlays = () => {
 let index = 0;
 let poster_master_play = document.getElementById('poster_master_play');
 let masterSongName = document.getElementById('masterSongName');
+let downloadMusic = document.getElementById('downloadMusic');
+
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((e) => {
     e.addEventListener('click', (event) => {
         index = parseInt(event.target.id);
@@ -134,22 +217,27 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((e) => {
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
         poster_master_play.src = songs[index].coverPath;
+        downloadMusic.href = songs[index].path;
         gif.style.opacity = 1;
+        poster_master_play.style.opacity = 1;
 
         let songTitles = songs.filter((els) => {
             return els.id == index + 1;
         });
         songTitles.forEach(elss => {
-            // let {name} = elss;
             masterSongName.innerHTML = elss.name;
+            downloadMusic.setAttribute('download', elss.name);
         });
 
 
         makeAllBackgrounds();
         Array.from(document.getElementsByClassName('pop_item'))[index].style.background = "rgba(105, 105, 170, 1)";
+        Array.from(document.getElementsByClassName('pop_item'))[index].getElementsByClassName('subtitle')[0].style.color = "black";
+
         makeAllPlays();
         event.target.classList.remove('fa-play-circle');
         event.target.classList.add('fa-pause-circle');
+
 
     })
 });
@@ -166,19 +254,19 @@ music.addEventListener('timeupdate', () => {
 
     let min_curr = Math.floor(music_curr / 60);
     let sec_curr = Math.floor(music_curr % 60);
-    if(sec_curr < 10) {
+    if (sec_curr < 10) {
         sec_curr = `0${sec_curr}`;
     }
-    if(min_curr < 10) {
+    if (min_curr < 10) {
         min_curr = `0${min_curr}`;
     }
 
     let min_dur = Math.floor(music_dur / 60);
     let sec_dur = Math.floor(music_dur % 60);
-    if(sec_dur < 10) {
+    if (sec_dur < 10) {
         sec_dur = `0${sec_dur}`;
     }
-    if(min_dur < 10) {
+    if (min_dur < 10) {
         min_dur = `0${min_dur}`;
     }
 
@@ -186,9 +274,9 @@ music.addEventListener('timeupdate', () => {
     currentEnd.innerHTML = `${min_dur}:${sec_dur}`;
 
 
-    let progressBar = ((music_curr / music_dur)* 100);
+    let progressBar = ((music_curr / music_dur) * 100);
     seek.value = progressBar;
-    let seekBar  = seek.value;
+    let seekBar = seek.value;
     bar2.style.width = `${seekBar}%`;
     dot.style.left = `${seekBar}%`;
 
@@ -213,17 +301,17 @@ vol.addEventListener('input', () => {
     vol_bar.style.width = `${volVal}%`;
     vol_dot.style.left = `${volVal}%`;
 
-    if(vol.value == 0) {
+    if (vol.value == 0) {
         vol_icon.classList.remove('fa-volume-low');
         vol_icon.classList.remove('fa-volume-high');
         vol_icon.classList.add('fa-volume-mute');
     }
-    if(vol.value > 0) {
+    if (vol.value > 0) {
         vol_icon.classList.remove('fa-volume-high');
         vol_icon.classList.remove('fa-volume-mute');
         vol_icon.classList.add('fa-volume-low');
     }
-    if(vol.value > 50) {
+    if (vol.value > 50) {
         vol_icon.classList.remove('fa-volume-mute');
         vol_icon.classList.remove('fa-volume-low');
         vol_icon.classList.add('fa-volume-high');
@@ -235,7 +323,7 @@ let next = document.getElementById('next');
 
 next.addEventListener('click', () => {
     index += 1;
-    if(index > 9) {
+    if (index > 9) {
         index = 0;
     }
     music.src = songs[index].path;
@@ -244,19 +332,19 @@ next.addEventListener('click', () => {
     masterPlay.classList.add('fa-pause-circle');
     poster_master_play.src = songs[index].coverPath;
     gif.style.opacity = 1;
+    poster_master_play.style.opacity = 1;
 
     let songTitles = songs.filter((els) => {
         return els.id == index + 1;
     });
     songTitles.forEach(elss => {
-        // let {name} = elss;
         masterSongName.innerHTML = elss.name;
     });
 });
 
 back.addEventListener('click', () => {
     index -= 1;
-    if(index < 0) {
+    if (index < 0) {
         index = 9;
     }
     music.src = songs[index].path;
@@ -265,6 +353,7 @@ back.addEventListener('click', () => {
     masterPlay.classList.add('fa-pause-circle');
     poster_master_play.src = songs[index].coverPath;
     gif.style.opacity = 1;
+    poster_master_play.style.opacity = 1;
 
     let songTitles = songs.filter((els) => {
         return els.id == index + 1;
@@ -300,3 +389,132 @@ pop_art_left.addEventListener('click', () => {
 pop_art_right.addEventListener('click', () => {
     pop_art.scrollLeft += 200;
 });
+
+
+let shuffle = document.getElementsByClassName('shuffle')[0];
+shuffle.addEventListener('click', () => {
+    let a = shuffle.innerHTML;
+    switch (a) {
+        case "next":
+            shuffle.innerHTML = "repeat";
+            shuffle.classList.remove('fa-music');
+            shuffle.classList.remove('fa-shuffle');
+            shuffle.classList.add('fa-repeat');
+            break;
+        case "repeat":
+            shuffle.innerHTML = "random";
+            shuffle.classList.remove('fa-music');
+            shuffle.classList.remove('fa-repeat');
+            shuffle.classList.add('fa-shuffle');
+            break;
+        case "random":
+            shuffle.innerHTML = "next";
+            shuffle.classList.remove('fa-shuffle');
+            shuffle.classList.remove('fa-repeat');
+            shuffle.classList.add('fa-music');
+            break;
+    }
+});
+
+music.addEventListener('ended', () => {
+    let a = shuffle.innerHTML;
+    switch (a) {
+        case "next":
+            index += 1;
+            if (index > 9) {
+                index = 0;
+            }
+            music.src = songs[index].path;
+            music.play();
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
+            poster_master_play.src = songs[index].coverPath;
+            gif.style.opacity = 1;
+            poster_master_play.style.opacity = 1;
+
+            let songTitles = songs.filter((els) => {
+                return els.id == index + 1;
+            });
+            songTitles.forEach(elss => {
+                masterSongName.innerHTML = elss.name;
+            });
+            break;
+        case "repeat":
+            music.play();
+            break;
+        case "random":
+            index = Math.floor(Math.random() * 10);
+            music.src = songs[index].path;
+            music.play();
+            masterPlay.classList.remove('fa-play-circle');
+            masterPlay.classList.add('fa-pause-circle');
+            poster_master_play.src = songs[index].coverPath;
+            gif.style.opacity = 1;
+            poster_master_play.style.opacity = 1;
+
+            let songTitles1 = songs.filter((els) => {
+                return els.id == index + 1;
+            });
+            songTitles1.forEach(elss => {
+                masterSongName.innerHTML = elss.name;
+            });
+            break;
+    }
+});
+
+
+
+let artists=[
+    {
+        id: 1,
+        name: `Ed Sheeran`,
+        coverPath: 'Assets/Artist/ed_sheeran.jpg',
+    },
+    {
+        id: 2,
+        name: `Charlie Puth`,
+        coverPath: 'Assets/Artist/charlie_puth.jpeg',
+    },
+    {
+        id: 3,
+        name: `Arjit Singh`,
+        coverPath: 'Assets/Artist/arjit_singh.jpeg',
+    },
+    {
+        id: 4,
+        name: `Taylor Swift`,
+        coverPath: 'Assets/Artist/taylor_swift.jpg',
+    },
+    {
+        id: 5,
+        name: `The Chainsmokers`,
+        coverPath: 'Assets/Artist/the_chainsmokers.jpg',
+    },
+    {
+        id: 6,
+        name: `One Direction`,
+        coverPath: 'Assets/Artist/one_direction.jpg',
+    },
+    {
+        id: 7,
+        name: `Shawn Mendes`,
+        coverPath: 'Assets/Artist/shawn_mendes.jpg',
+    },
+    {
+        id: 8,
+        name: `Pritam`,
+        coverPath: 'Assets/Artist/pritam.jpeg',
+    },
+    {
+        id: 9,
+        name: `Atif Aslam`,
+        coverPath: 'Assets/Artist/atif_aslam.jpg',
+    },
+
+
+]
+
+Array.from(document.getElementsByClassName('art_item')).forEach((element, i) => {
+    element.getElementsByTagName('img')[0].src = artists[i].coverPath;
+    element.getElementsByTagName('h5')[0].innerHTML = artists[i].name;
+})
